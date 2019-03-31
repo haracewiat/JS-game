b6b4bbab25f3049653eb5284c34f11378dd71377
Beata Haracewiat <b.haracewiat@lancaster.ac.uk>
2019-03-31 15:21:29 +0100

    Added a ball
    Added a ball to the game. The ball will bounce from each side of the canvas as well as from the top of the paddle.
    
    > the game needs to end when the ball touches the bottom of the canvas
    > bricks need to be added

--- index.js
+++ index.js
@@ -7,16 +7,30 @@ const CANVAS_HEIGHT = canvas.height;
 const PADDLE_WIDTH = 60;
 const PADDLE_HEIGHT = 4;
 
+const BALL_HEIGHT = 3;
+const BALL_WIDTH = BALL_HEIGHT*2.5;
+const BALL_SPEED = 2.5;
 
 var paddle = {
 
     height: PADDLE_HEIGHT,
     width: PADDLE_WIDTH,
     x: CANVAS_WIDTH/2 - PADDLE_WIDTH/2,
-    y: CANVAS_HEIGHT - PADDLE_HEIGHT - 10,
+    y: CANVAS_HEIGHT - PADDLE_HEIGHT - 6,
     x_velocity: 0
   
 };
+
+var ball = {
+
+    height: BALL_HEIGHT,
+    width: BALL_WIDTH,
+    x: CANVAS_WIDTH/2,
+    y: CANVAS_HEIGHT - PADDLE_HEIGHT - 6 - BALL_HEIGHT,
+    x_velocity: BALL_SPEED,
+    y_velocity: BALL_SPEED 
+
+}
 
 var controller = {
 
@@ -41,28 +55,55 @@ var controller = {
 
 var loop = function() {
 
-   
+//move the paddle
     if(controller.left){  
-      paddle.x_velocity -= 1.5;  
+    paddle.x_velocity -= 1.5;  
+    }else if(controller.right){  
+    paddle.x_velocity += 1.5;  
     }
-  
-    if(controller.right){  
-      paddle.x_velocity += 1.5;  
-    }
- 
-   
+
     paddle.x += paddle.x_velocity;
     paddle.x_velocity *= 0.8;                      
   
-   
+//create boundries for the paddle (don't let it fall out of the canvas)
     if(paddle.x < 0){  
         paddle.x = 0;
     }else if(paddle.x > CANVAS_WIDTH - PADDLE_WIDTH){
         paddle.x = CANVAS_WIDTH - PADDLE_WIDTH;
     }
 
+//move the ball
+    ball.x += ball.x_velocity;
+    ball.y += ball.y_velocity;
+
+//create boundries for the ball (don't let it fall out of the canvas)
+    //top and bottom
+    if(ball.y > CANVAS_HEIGHT - BALL_HEIGHT || ball.y < 0 + BALL_HEIGHT){
+        ball.y_velocity = -ball.y_velocity;
+    }
+
+    //sides
+    if(ball.x > CANVAS_WIDTH - BALL_WIDTH || ball.x < 0  + BALL_WIDTH){  
+        ball.x_velocity = -ball.x_velocity;
+    }
+
+    //paddle collision
+    let ballBottom = ball.y + ball.height/2;
+    let paddleTop = paddle.y;
+    let paddleLeft = paddle.x;
+    let paddleRight = paddle.x + paddle.width;
+
+    if(ballBottom > paddleTop && ball.x + ball.width/2 >= paddleLeft && ball.x - ball.width/2 <= paddleRight){
+        ball.y_velocity = -ball.y_velocity;
+        ball.y =  paddle.y - ball.height/2;
+    }
+    
+    
+    
+    
   
-    //draw the game    
+//draw the game    
+
     //background for the game
     game.fillStyle = "#202020";
     game.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
@@ -73,14 +114,21 @@ var loop = function() {
     game.rect(paddle.x, paddle.y, paddle.width, paddle.height);
     game.fill();
 
-    
-    
-    //call update when the browser is ready to draw again
+    //ball
+    game.fillStyle = "#db4fb8";
+    game.beginPath();
+    game.ellipse(ball.x, ball.y, ball.width, ball.height, 0, 0, 2 * Math.PI);
+    game.fill();
+
+        
+
+//call update when the browser is ready to draw again
     window.requestAnimationFrame(loop);
     
-  };
+};
 
-window.addEventListener("keydown", controller.keyListener)
+window.addEventListener("keydown", controller.keyListener);
 window.addEventListener("keyup", controller.keyListener);
+
 window.requestAnimationFrame(loop);
 
