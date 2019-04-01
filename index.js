9f6673208210b8082fd9aa94db12ddbb9565f8c4
Beata Haracewiat <b.haracewiat@lancaster.ac.uk>
2019-04-01 19:23:28 +0100

    Created a brick
    Created a new object brick.
    
    > needs a function to populate the array and draw a brick if the array contains one in a given index

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ var game = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width;
 const CANVAS_HEIGHT = canvas.height;
 
+const SIDE_OFFSET = 15;
+const TOP_OFFSET = 5;
+
 const PADDLE_WIDTH = 60;
 const PADDLE_HEIGHT = 4;
 
@@ -11,6 +14,13 @@ const BALL_HEIGHT = 3;
 const BALL_WIDTH = BALL_HEIGHT*2.5;
 const BALL_SPEED = 2.5;
 
+const BRICKS_ROW = 6;
+const BRICKS_COLUMN = 9;
+const BRICK_WIDTH = (CANVAS_WIDTH-SIDE_OFFSET*2)/BRICKS_ROW;
+const BRICK_HEIGHT = 7;
+
+
+//objects
 var paddle = {
 
     height: PADDLE_HEIGHT,
@@ -29,6 +39,16 @@ var ball = {
     y: CANVAS_HEIGHT - PADDLE_HEIGHT - 6 - BALL_HEIGHT,
     x_velocity: BALL_SPEED,
     y_velocity: BALL_SPEED 
+
+}
+
+var brick = {
+
+    height: BRICK_HEIGHT,
+    width: BRICK_WIDTH,
+    x: SIDE_OFFSET,
+    y: TOP_OFFSET,
+    delete: 0
 
 }
 
@@ -52,6 +72,17 @@ var controller = {
     }
 
 };
+
+let bricks = [];
+
+
+
+
+
+function randomColor() {
+    return '#' + Math.floor(Math.random()*0xffffff).toString(16);
+}
+
 
 var loop = function() {
 
@@ -98,14 +129,19 @@ var loop = function() {
         ball.y =  paddle.y - ball.height/2;
     }
     
-    
-    
-    
+    //brick collision
+    let ballTop = ball.y - ball.height/2;
+    let brickBottom = brick.y + BRICK_HEIGHT;
+
+    if(ballTop < brickBottom){
+        ball.y_velocity = -ball.y_velocity;
+        ball.y =  brick.y + BRICK_HEIGHT + ball.height/2;        
+    }
   
 //draw the game    
 
     //background for the game
-    game.fillStyle = "#202020";
+    game.fillStyle = "rgba(32, 32, 32, 1)";
     game.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
     //paddle
@@ -120,6 +156,24 @@ var loop = function() {
     game.ellipse(ball.x, ball.y, ball.width, ball.height, 0, 0, 2 * Math.PI);
     game.fill();
 
+    //brick
+    game.fillStyle = "#dfa6ea";
+    game.beginPath();
+    //game.rect(brick.x, brick.y, brick.width, brick.height);
+    
+
+    for(let i=0; i<BRICKS_ROW; i++){
+        for(let j=0; j<BRICKS_COLUMN; j++){
+            if(brick.delete == 0){
+                bricks[i*j] = new brick(SIDE_OFFSET+i*BRICK_WIDTH, TOP_OFFSET+j*BRICK_HEIGHT);
+                console.log(bricks[i*j].x);
+                //bricks.push(game.rect(bricks[i*j].x, bricks[i*j].y, brick.width, brick.height));
+            }
+
+        }
+    }
+
+    game.fill();
         
 
 //call update when the browser is ready to draw again
